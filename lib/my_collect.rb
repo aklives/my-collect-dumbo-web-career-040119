e0500e7c6853022253c5a3f71831af30888871d2
Adam Kearney <aklives@hotmail.com>
2019-03-16 10:06:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,13 @@
 def my_collect(array)
-  new_arr = [] 
-  i=0 
-  while i < array.length 
+  new_arr = []
+  i=0
+  while i < array.length
     new_arr << yield(array[i])
-    i += 1 
-  end 
-  for element in array 
-    new_arr << yield element 
-  end 
-  
+    i += 1
+  end
+  for element in array
+    new_arr << yield element
+  end
+
   new_arr
-end 
+end
